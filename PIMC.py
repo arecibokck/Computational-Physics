a0d0089a4eb838ec9650909850e33c89dc05f662
Karthik <arecibokck@gmail.com>
2017-03-13 20:46:15 +0100

    Corrections and Updates

--- PIMC.py
+++ PIMC.py
@@ -2,7 +2,6 @@ import numpy as np
 from scipy.stats import gaussian_kde
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
-import csv
 
 class Path(object):
 
@@ -36,44 +35,44 @@ class Path(object):
 
     def V(self,x):
 
-        return 0.5*(self.mu**2)*(x**2) + self.lam*((x**2-self.f**2)**2) #Potential Energy(PE)
-                                                                        #lam = 0 -> Harmonic Oscillator
-                                                                        #lam > 0 -> Anharmonic Oscillator
+        return 0.5*(self.mu**2)*(x**2) + self.lam*((x**2-self.f**2)**2) # Potential Energy(PE)
+                                                                        # lam = 0 -> Harmonic Oscillator
+                                                                        # lam > 0 -> Anharmonic Oscillator
 
     def DV(self,x):
 
-        return (self.mu**2)*x + 4*self.lam*x*(x**2-self.f**2)
+        return (self.mu**2)*x + 4*self.lam*x*(x**2-self.f**2) # First derivative of Potential Energy
 
     def K(self,x):
 
-        return 0.5*self.m*(x**2)*(1/self.dT**2) #Kinetic Energy(KE) with m = 1
+        return 0.5*self.m*(x**2)*(1/self.dT) # Kinetic Energy(KE) with m = 1
 
     def E(self,x):
 
         return self.V(x) + (0.5*x*self.DV(x)) # Energy from Virial Theorem - V+(0.5*x*dV/dx)
 
 
-def mcstep(path): #One Markov Chain Monte Carlo Step - Metropolis Algorithm
+def mcstep(path): # One Markov Chain Monte Carlo Step - Metropolis Algorithm
 
-    k = int(path.M * np.random.uniform()) #Pick an arbitrary index
-    x_p = path.X[k] + np.random.uniform(-path.delta_x,path.delta_x) #Pick the position corresponding to random index and..
-    k_p = k + 1                                                  #..generate new value by adding a small arbitrary change in value, delta_x.
+    k = int(path.M * np.random.uniform()) # Pick an arbitrary index
+    x_p = path.X[k] + np.random.uniform(-path.delta_x,path.delta_x) # Pick the position corresponding to random index and..
+    k_p = k + 1                                                  # ..generate new value by adding a small arbitrary change in value, delta_x.
     if (k_p > path.M-1): k_p = 0
     k_m = k - 1
     if (k_m < 0): k_m = path.M-1
-    dVa = path.V(x_p) - path.V(path.X[k]) #Potential Action
+    dVa = path.V(x_p) - path.V(path.X[k]) # Potential Action
     dKa = (path.K(path.X[k_p]-x_p) + path.K(x_p - path.X[k_m])) - (path.K(path.X[k_p] - path.X[k]) + path.K(path.X[k] - path.X[k_m])) #Kinetic Action
     dS = path.dT*(dVa + dKa) # Total Action for Harmonic Oscillator
-    if(dS < 0.0 or np.random.normal(0,1) < np.exp(-dS)): #Accept-Reject step
+    if(dS < 0.0 or np.random.normal(0,1) < np.exp(-dS)): # Accept-Reject step
         x_new = path.X[k] = x_p
     else:
         x_new = path.X[k]
 
-    return x_new #New position as determined by the acceptance probability
+    return x_new # New position as determined by the acceptance probability
 
 def render_plots(ms, x, pdf):
 
-    #Plot of MS values of position
+    # Plot of MS values of position
     f = plt.figure(1,figsize=(10, 5), dpi=80)
     fs = gridspec.GridSpec(1, 2, width_ratios=[1,1])
     fs.update(wspace = 0.3)
@@ -89,7 +88,7 @@ def render_plots(ms, x, pdf):
     plt.plot(m, label='Mean')
     plt.legend(prop={'size':10})
 
-    #Histogram of the probability density
+    # Plot of Probability density function
     plt.subplot(fs[1])
     plt.xlabel('Position in x')
     plt.ylabel('Probability')
@@ -100,29 +99,22 @@ def render_plots(ms, x, pdf):
     plt.legend(prop={'size':10})
     plt.show()
 
-def dump_data(pdf,ms,energy):
-
-    with open('PIMC_data.csv', 'w') as fp:
-        a = csv.writer(fp, delimiter=',')
-        data = [pdf, ms, energy]
-        a.writerows(data)
-
 def PIMC(path):
 
-    if(path.thermalize): #Thermalize
+    if(path.thermalize): # Thermalize
         print("Running Thermalization Steps...")
-        for i in range(path.n_steps/2): #Run over a few steps for the first time to pass burn-in phase
+        for i in range(path.n_steps/2): # Run over a few steps for the first time to pass burn-in phase
             for j in range(path.M): # Run over the full time = dT*M
                 mcstep(path)
 
-    #Production Steps
+    # Production Steps
     print("Running Production Steps...")
     ms = []
     energy = []
     ensemble_pdf = [0]*path.n_bins
     for i in range(path.n_steps): # Run over the full range of steps to obtain energy values that can be worked with
         ms.append(np.mean(np.square(path.X))/path.T)
-        for j in range(path.M): #Run over the full time = dT*M
+        for j in range(path.M): # Run over the full time = dT*M
             x_new = mcstep(path)
             bins = abs(int((x_new - path.x_min)/ (path.x_max - path.x_min) * path.n_bins))
             if(bins<path.n_bins):ensemble_pdf[bins]+=1
@@ -133,11 +125,8 @@ def PIMC(path):
     for i in range(len(ensemble_pdf)):
         a.append([x[i]]*ensemble_pdf[i])
     y = np.array([j for i in a for j in i])
-    kde = gaussian_kde(y, bw_method=0.2 / y.std(ddof=1)) #Kernel Density Estimation to determine PDF
+    kde = gaussian_kde(y, bw_method=0.2 / y.std(ddof=1)) # Kernel Density Estimation to determine PDF
     kde.covariance_factor = lambda : .25
     kde._compute_covariance()
-    pdf = kde.evaluate(x) #Normalized pdf of the ensemble of paths
+    pdf = kde.evaluate(x) # PDF of the ensemble of paths
     render_plots(ms, x, pdf)
-    print("Dumping all data into a CSV file...")
-    dump_data(pdf, ms, energy)
-    print("Finished! - All tasks successfully completed!")
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from PIMC import *
+import csv
 
 #Initialize
 M = 100 #Number of time slices
@@ -9,7 +10,7 @@ mu = 1 #Oscillator Frequency?
 f = 4.0 #Arbitrary Constant? for Anharmonic Oscillator
 char = 'c' #Choose Hot = 'h' or Cold = 'c' Start
 thermalize = False #Set True to run Thermalization to pass burn-in phase
-lam = 1.0 #Quartic Coupling Constant - lam = 0 -> Harmonic Oscillator; lam > 0 -> Anharmonic Oscillator
+lam = 0.0 #Quartic Coupling Constant - lam = 0 -> Harmonic Oscillator; lam > 0 -> Anharmonic Oscillator
 n_steps = 1000  #Number of Monte Carlo steps
 x_max = 8.0 #Max value corresponding to the last bin
 n_bins = 100 #Number of bins to draw the probability density histogram over
@@ -17,4 +18,14 @@ n_bins = 100 #Number of bins to draw the probability density histogram over
 path = Path(M,T,delta_x,m,mu,f,char,thermalize,lam,n_steps,x_max,n_bins)
 
 if __name__=='__main__':
+
     PIMC(path)
+#    for i in range():
+
+#    with open('PIMC_data.csv', 'w') as fp:
+#        a = csv.writer(fp, delimiter=',')
+#        data = []
+#        a.writerows(data)
+#    print("Dumping all data into a CSV file...")
+#    dump_data()
+    print("Finished! - All tasks successfully completed!")
