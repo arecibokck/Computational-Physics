1ebe20517f7f3d3e8ddef4e7ebc45f28f592bc1e
Karthik <arecibokck@gmail.com>
2017-03-15 00:21:12 +0100

    Corrections and Updates

--- data_analysis.R
+++ data_analysis.R
@@ -21,13 +21,19 @@ for (i in 1:length(msp)) {
 }
 
 acorr <- acf(mspvector, 200, plot=F)
-
 plot(acorr, main = 'Autocorrelation of mean squared position values', xlab  = 'Monte Carlo Time')
 
 acfdata <- data.frame(lag=acorr$lag, acf=acorr$acf)
 
 acorr.log <- log(acfdata[,2])
+x <- seq(length(acorr.log))
+#loess_fit <- loess(acorr.log ~ x)
+plot(acorr.log, type='o' , pch=1, cex=.2 , col = 'steelblue2', main = 'Log autocorrelation of mean squared position values', ylab = 'Log(ACF)', xlab  = 'Monte Carlo Time')
+fit <- lm(acorr.log ~ x)
+abline(coef(fit)[1:2])
+cf <- round(coef(fit), 2)
+eq <- paste0("log(acorr) = ", cf[1], ifelse(sign(cf[2])==1, " + ", " - "), abs(cf[2]), " x ")
+mtext(eq, 3, line=-2)
+#lines(predict(loess_fit ), col='red', lwd=2)
 
-plot(acorr.log, main = 'Log autocorrelation of mean squared position values', ylab = 'Log ACF', xlab  = 'Monte Carlo Time')
-
-write.table(acfdata, 'acfdata.txt', sep='\t')
+# write.table(acfdata, 'acfdata.txt', sep='\t')
